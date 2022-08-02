de845caefe90b3048ebbc7bae2b641cdcdad15d7
peolivier <peolivier.dev@outlook.com>
2022-08-01 21:54:01 -0400

    feat: add ci create semantic-release

--- release.conf.js
+++ release.conf.js
@@ -1,9 +1,9 @@
-module.exports = {
-  branches: ["main"],
-  repositoryUrl: "https://github.com/peolivier/learnGithubActions_ReactApp",
-  plugins: [
-    '@semantic-release/commit-analyzer', 
-    '@semantic-release/release-notes-generator', 
-    // '@semantic-release/npm', // we already have it
-    '@semantic-release/github']
-}
+// module.exports = {
+//   branches: ["main"],
+//   repositoryUrl: "https://github.com/peolivier/learnGithubActions_ReactApp",
+//   plugins: [
+//     '@semantic-release/commit-analyzer', 
+//     '@semantic-release/release-notes-generator', 
+//     // '@semantic-release/npm', // we already have it
+//     '@semantic-release/github']
+// }
