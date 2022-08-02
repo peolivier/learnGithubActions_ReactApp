16281d6e21da159a06081514e1c8528a0e88cb14
peolivier <peolivier.dev@outlook.com>
2022-08-01 21:55:18 -0400

    chore: reformat for Prettier

--- release.conf.js
+++ release.conf.js
@@ -2,8 +2,8 @@
 //   branches: ["main"],
 //   repositoryUrl: "https://github.com/peolivier/learnGithubActions_ReactApp",
 //   plugins: [
-//     '@semantic-release/commit-analyzer', 
-//     '@semantic-release/release-notes-generator', 
+//     '@semantic-release/commit-analyzer',
+//     '@semantic-release/release-notes-generator',
 //     // '@semantic-release/npm', // we already have it
 //     '@semantic-release/github']
 // }
