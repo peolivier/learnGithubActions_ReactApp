3a89f5818ab0a6f00eecf98ab1fbfbfe88af1ac9
peolivier <peolivier.dev@outlook.com>
2022-08-05 17:06:02 -0400

    fix(ci): set env variable from docker action hello

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh -l
 
-echo "::debug ::Debug Message" # Format telling to use a Log format
-echo "::warning ::Warning Message" # Format telling to use a Log format
-echo "::error ::Error Message" # Format telling to use a Log format
+if false
+then
+    echo 'error'
+    exit 1 # This is how you tells that it fails!
+fi
+echo "::debug ::Debug Message - docker action" # Format telling to use a Log format
+echo "::warning ::Warning Message - docker action" # Format telling to use a Log format
+echo "::error ::Error Message - docker action" # Format telling to use a Log format
 
 echo "::add-mask::$1"
 echo "Hello $1"
@@ -15,4 +20,6 @@ echo "some stuff#2"
 echo "some stuff#3"
 echo "::endgroup::s"
 
-echo "::set-env name=HELLO::hello"
+# echo "::set-env name=HELLO::hello" # THIS IS DISABLE/DEPRECATED
+# see https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-environment-variable
+echo "HELLO=hello" >> $GITHUB_ENV
